e8ad36982a613585a09c3510fe77487386bc9e05
Vefa Caglar Ozcan <vefacaglarozcan@gmail.com>
2026-02-26 21:55:05 +0300

    setting up ui elements

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,10 +10,19 @@ public class PlayerController : MonoBehaviour
 
     public float speed = 0.0f;
 
+    private int count;
+
+    public TextMeshProUGUI countText;
+
+    public GameObject winTextObject;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        winTextObject.SetActive(false);
+        count = 0;
+        SetCountText();
     }
 
     void OnMove(InputValue movementValue)
@@ -36,6 +46,22 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Pickup"))
         {
             other.gameObject.SetActive(false);
+            IncreaseCount();
+        }
+    }
+
+    private void IncreaseCount()
+    {
+        count = count + 1;
+        SetCountText();
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winTextObject.SetActive(true);
         }
     }
 }
