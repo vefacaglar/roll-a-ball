dd0c27db4e4863e7753686d532fd134fd3ace274
Vefa Caglar Ozcan <vefacaglarozcan@gmail.com>
2026-02-26 19:28:01 +0300

    move sphere with force

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,31 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
+    public float speed = 0.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+         rb = GetComponent<Rigidbody>();
     }
 
-    void OnMove()
+    void OnMove(InputValue movementValue)
     {
-        
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+
     }
 }
